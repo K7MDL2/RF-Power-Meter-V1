04c18f3b20074b571663ac54540a20e63e7a5eae
K7MDL2 <64443004+K7MDL2@users.noreply.github.com>
2020-04-28 20:15:43 -0400

    Arduino Setup

--- Watt_Meter V1.00.ino
+++ Watt_Meter V1.00.ino
@@ -1,7 +1,7 @@
 /*
  * 
  *
- Watt meter by K7MDL 4/22/2020
+ Watt meter by K7MDL 4/22/2020   
  *
  Displays Forward Power in Watts on analog scale, displays Forward and Reflected power in Watts and dBm
  in digital form, and calculates SWR and displays in digital and analog meter form
